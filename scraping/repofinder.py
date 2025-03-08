fcd7d9629f3f19b78486a36b9d40d3e4f76435b8
juanis2112 <juanitagomezr2112@gmail.com>
2025-03-08 14:04:27 -0800

    Fix adding output folders if they dont exist

--- scraping/repofinder.py
+++ scraping/repofinder.py
@@ -37,6 +37,8 @@ def repo_finder(env, path, headers):
     query_terms, university_acronym = build_queries(env)
     repositories = search_repositories_with_queries(query_terms, headers)
     logger.info(f"Total repositories found: {len(repositories)}")
+    os.makedirs('Data/json', exist_ok=True)
+    os.makedirs('Data/db', exist_ok=True)
     output_filename_json = f"{path}/Data/json/repository_data_{university_acronym}.json"
     
     with open(output_filename_json, 'w', encoding='utf-8') as f:
