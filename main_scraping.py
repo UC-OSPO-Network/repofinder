f40241b824f280fc5ebb5836b887c85073c8ebb2
juanis2112 <juanitagomezr2112@gmail.com>
2026-01-24 13:59:49 -0800

    Update main scraping

--- main_scraping.py
+++ main_scraping.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from repofinder.scraping.get_repositories import get_repositories
-from repofinder.scraping.json_to_db import create_and_populate_database
+from repofinder.scraping.search_repositories import search_repositories
+from repofinder.scraping.search_organizations import search_organizations
+from repofinder.scraping.search_users import search_users
+from repofinder.scraping.repo_scraping_utils import get_repositories_from_organizations, get_repositories_from_users
+from repofinder.scraping.json_to_db import create_and_populate_database, populate_organizations, populate_users
 from repofinder.scraping.get_contributors import get_contributor_data
 from repofinder.scraping.get_organizations import get_organization_data
 from repofinder.scraping.get_repo_extras import get_features_data
@@ -15,63 +18,70 @@ load_dotenv(DOTENV)
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 HEADERS = {
     'Authorization': f'token {GITHUB_TOKEN}',
-    'Accept': 'application/vnd.github.v3+json'
+    'Accept': 'application/vnd.github.v3+json',
 }
 
 FEATURES = [
-    "release_downloads",
     "readme",
-    "code_of_conduct",
+    "subscribers_count",
+    "release_downloads",
+    "code_of_conduct_file",
     "contributing",
-    "security_policy",
     "issue_templates",
     "pull_request_template",
-    "subscribers_count",
+    "security_policy",
 ]
 
-def scrape(university_acronyms=["UCSD"]):
+def scrape(university_acronyms=["UCSB", "UCSC", "UCSD"]):
     for acronym in university_acronyms:
     
         config_file= f"config/config_{acronym}.json"
         repo_file=f"Data/json/repository_data_{acronym}.json"
+        org_file=f"Data/json/organization_data_{acronym}.json"
+        repo_from_orgs_file=f"Data/json/repository_data_org_scraping_{acronym}.json"
+        user_file=f"Data/json/user_data_{acronym}.json"
+        repo_from_users_file=f"Data/json/repository_data_user_scraping_{acronym}.json"
         db_file = f"Data/db/repository_data_{acronym}_database.db"
     
-        # Scraping
-        times = []  
-        
-        start_time = time.time()
         print('Finding repositories')
-        get_repositories(config_file, HEADERS)
-        end_time = time.time()
-        times.append(end_time - start_time)
+        search_repositories(config_file, HEADERS)
         print('Repositories collected')
-        
-        # start_time = time.time()
-        create_and_populate_database(repo_file, db_file)
-        end_time = time.time()
-        times.append(end_time - start_time)
+
+        create_and_populate_database(repo_file, db_file, search_method='repository_search')
         print('Database populated done')
         
-        start_time = time.time()
+        print('Finding organizations')
+        search_organizations(config_file, HEADERS)
+        populate_organizations (org_file, db_file)
+        print('Organizations collected')
+
+        print('Finding repositories from organizations')
+        get_repositories_from_organizations(acronym, org_file, HEADERS)
+        print('Repositories from organizations collected')
+        
+        create_and_populate_database(repo_from_orgs_file, db_file, search_method='organization_search')
+        print('Repositories from organizations populated in database')
+        
+        print('Finding users')
+        search_users(config_file, HEADERS)
+        populate_users(user_file, db_file)
+        print('Users collected')
+        
+        print('Finding repositories from users')
+        get_repositories_from_users(acronym, user_file, HEADERS)
+        print('Repositories from users collected')
+        
+        create_and_populate_database(repo_from_users_file, db_file, search_method='user_search')
+        print('Repositories from users populated in database')
+
+        get_features_data(repo_file, db_file, HEADERS, FEATURES)
+        print('Extra features done', end_time - start_time)
+
         get_organization_data(repo_file, db_file, HEADERS)
-        end_time = time.time()
-        times.append(end_time - start_time)
         print('Organizations done')
         
-        start_time = time.time()
-        get_features_data(repo_file, db_file, HEADERS, FEATURES)
-        end_time = time.time()
-        times.append(end_time - start_time)
-        print('Extra features done')
-        
-        start_time = time.time()
         get_contributor_data(repo_file, db_file, HEADERS)
-        end_time = time.time()
-        times.append(end_time - start_time)
         print('Contributors done')
-    
-        # Print execution times
-        print("Execution times (seconds):", times)
 
 if __name__ == "__main__":
     scrape()
